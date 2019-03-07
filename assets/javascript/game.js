c34baa327c1bdbfd99fb759a316979aa05de51f8
Mike Slaby <mikeslaby3@gmail.com>
2019-03-06 19:38:01 -0800

    comp generates random letter

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+// Creates an array that lists computer options a-z
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
+
+// This function is run whenever the user presses a key.
+document.onkeyup = function(event) {
+
+    // Determines which key was pressed.
+    var userGuess = event.key;
+
+    // Randomly chooses a choice from the options array. This is the Computer's guess.
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    // Alerts the key the user pressed (userGuess).
+    console.log("User guess: " + userGuess);
+
+    // Alerts the Computer's guess.
+    console.log("Computer guess: " + computerGuess);
+
+}
