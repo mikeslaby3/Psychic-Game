1bc8de62f4fb5de18746e75a256eab6e2c1f4620
Mike Slaby <mikeslaby3@gmail.com>
2019-03-09 17:31:05 -0800

    Added conditional for only a-z picks and figured out guesses array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,43 +5,59 @@ var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var guesses = 0;
-
-// Create variables that hold references to the places in the HTML where we want to display things.
-var winsText = document.getElementById("wins-text");
-var lossedText = document.getElementById("losses-text");
-var guessesLeftText = document.getElementById("guesses-left-text");
-var guessesText = document.getElementById("guesses-text");
+var guesses = [];
 
 // Randomly chooses a choice from the options array. This is the Computer's guess.
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-console.log(computerGuess);
+var computerPick = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log(computerPick);
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
 
     console.log(event.key);
 
-    var userGuess = event.key;
+    var userPick = event.key;
 
-    console.log("checking if same", userGuess === computerGuess);
+    var aToZKey = false;
+    for (let index = 0; index < computerChoices.length; index++) {
+        if (userPick === computerChoices[index]) {
+            aToZKey = true;
+        }   
+    }
 
-    if (userGuess === computerGuess) {
-        wins++;
-        resetForNextTurn();
-    } else {
-        guessesLeft--;
-        console.log(guessesLeft);
-        if (guessesLeft === 0) {
+    if (aToZKey == true) {
+        guesses.push(userPick);
+
+        console.log("checking if same", userPick === computerPick);
+    
+        if (userPick === computerPick) {
+            wins++;
             resetForNextTurn();
-            losses++;
-        }
-    } 
+            guesses = [];
+        } else {
+            guessesLeft--;
+            console.log(guessesLeft);
+            if (guessesLeft === 0) {
+                resetForNextTurn();
+                losses++;
+                guesses = [];
+            }
+        } 
+    
+        document.getElementById("wins-text").textContent = wins;
+        document.getElementById("losses-text").textContent = losses;
+        document.getElementById("guesses-left-text").textContent = guessesLeft;
+        document.getElementById("guesses-text").textContent = guesses;
+    
+    
+    }
+    
+    // This function resets the game following a win or loss
+    function resetForNextTurn () {
+        guessesLeft = 9;
+        computerPick = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        console.log(computerPick);
+    }
+    }
 
-}
 
-function resetForNextTurn () {
-    guessesLeft = 9;
-    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    console.log(computerGuess);
-}
